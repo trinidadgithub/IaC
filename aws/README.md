6495613fed76d36e1470432d14c7e8cda5a8cc37
Trinidad Marroquin <trinidad.marroquin@gmail.com>
2024-10-08 10:29:41 -0500

    add aws

---
+++ aws/README.md
@@ -0,0 +1,5 @@
+# AWS install notes taken from https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html.  See this site for more information on upgrading
+# Run the following
+
+install-aws.sh
+
---
+++ aws/install-aws.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "------------------------"
+echo "Install aws cli from AWS"
+echo "------------------------"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+
