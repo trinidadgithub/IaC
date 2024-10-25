bf2b72d4f9883808b01339008fac2622f7573bdb
trinidadm <trinidad.marrorquin@gmail.com>
2024-10-25 09:00:56 -0500

    added azure storage account example

---
+++ azure/azure-storage-account/main.tf
@@ -0,0 +1,20 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "trinidad-resources"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "trinidadstorageacct"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.example.name
+}
